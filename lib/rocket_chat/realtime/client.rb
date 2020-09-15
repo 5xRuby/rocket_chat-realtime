67a6c468402e92d161e42e24e02f0b16f0564302
蒼時弦也 <elct9620@frost.tw>
2020-09-15 18:15:38 +0800

    Add client attributes for connector, adapter, driver

--- lib/rocket_chat/realtime/client.rb
+++ lib/rocket_chat/realtime/client.rb
@@ -1,16 +1,49 @@
 # frozen_string_literal: true
 
+require 'websocket/driver'
+
 module RocketChat
   module Realtime
     # Rocket.Chat Reamtime API
     #
     # @since 0.1.0
     class Client
+      # @since 0.1.0
+      attr_reader :server
+
       # @param options [Hash]
       #
       # @since 0.1.0
       def initialize(options = {})
-        # TODO
+        @server = options[:server]
+      end
+
+      # @return [String] the realtime api endpoint
+      #
+      # @since 0.1.0
+      def endpoint
+        "#{server}/websocket"
+      end
+
+      # @return [RocketChat::Realtime::Connector]
+      #
+      # @since 0.1.0
+      def connector
+        @connector ||= Connector.new(endpoint)
+      end
+
+      # @return [RocketChat::Realtime::Adapter]
+      #
+      # @since 0.1.0
+      def adapter
+        @adapter ||= Adapter.new(endpoint)
+      end
+
+      # @return [WebSocket::Driver::Client]
+      #
+      # @since 0.1.0
+      def driver
+        @driver ||= WebSocket::Driver.client(adapter)
       end
     end
   end
--- spec/rocket_chat/realtime/client_spec.rb
+++ spec/rocket_chat/realtime/client_spec.rb
@@ -3,5 +3,34 @@
 require 'spec_helper'
 
 RSpec.describe RocketChat::Realtime::Client do
-  pending
+  let(:options) do
+    {
+      server: 'wss://example.com'
+    }
+  end
+  let(:client) { described_class.new(options) }
+
+  describe '#endpoint' do
+    subject { client.endpoint }
+
+    it { is_expected.to eq('wss://example.com/websocket') }
+  end
+
+  describe '#connector' do
+    subject { client.connector }
+
+    it { is_expected.to be_a(RocketChat::Realtime::Connector) }
+  end
+
+  describe '#adapter' do
+    subject { client.adapter }
+
+    it { is_expected.to be_a(RocketChat::Realtime::Adapter) }
+  end
+
+  describe '#driver' do
+    subject { client.driver }
+
+    it { is_expected.to be_a(WebSocket::Driver::Client) }
+  end
 end
